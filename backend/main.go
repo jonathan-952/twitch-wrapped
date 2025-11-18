7f7774f3c4a6a6e5207171b8c0a5cd1c1cc14952
Jonathan <jonathan.s9522@gmail.com>
2025-11-17 22:29:47 -0500

    got api working, im not fw this go shi so far no bap

---
+++ backend/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"github.com/gin-gonic/gin"
+	"github.com/jonathan-952/twitch-wrapped/backend/controllers"
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	var (
+	OAUTH_TOKEN = os.Getenv("OAUTH_TOKEN")
+	TwitchClient = os.Getenv("CLIENT_ID")
+	)
+
+	router := gin.Default()
+
+	router.GET("/get_user/:user", controllers.NewGetTwitchUserHandler(OAUTH_TOKEN, TwitchClient))
+
+	router.Run()
+
+}
